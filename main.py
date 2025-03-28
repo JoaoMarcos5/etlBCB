5059bac454c92701e6188b42bb80fb8a821d6f65
JoaoMarcos5 <lucrandoo97@gmail.com>
2025-03-28 16:15:37 -0300

    Aula 2

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import requests
 import pandas as pd
+from src.extrectTransform import requestApiBcb
+from src.load import salvarCsv
 
-
-
-
-def requestApiBcb(data):
-    """
-    função para extrair os dados do meio de pagamento trimestrais do banco central
-    parametros:
-    data - ano - aaat(exemplo 20191)
-    """
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
-    req = requests.get(url)
-    dados = req.json()
-
-    df = pd.json_normalize(dados['value'])
-    return print(df)
-
-requestApiBcb('20191')
-
+dadosBcb = requestApiBcb('20191')
+salvarCsv(dadosBcb,'src/datasets/meiospagamentostri.csv', ";", ".")
---
+++ src/extrectTransform.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+
+
+
+
+def requestApiBcb(data) -> pd.DataFrame:
+    """
+    função para extrair os dados do meio de pagamento trimestrais do banco central
+    parâmetros:
+
+    data - string - aaat(exemplo 20191)
+
+    saída:
+    Dataframe - estrutura de dados do pandas 
+    """
+    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
+    req = requests.get(url)
+    print("status code", req.status_code)
+    dados = req.json()
+
+    df = pd.json_normalize(dados['value'])
+    df['datatrimestre'] = pd.to_datetime(df['datatrimestre'])
+
+
+
+    return df
+
+
+
+
