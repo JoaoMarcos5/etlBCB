dc64246607d1e050bd3b0d18227c829883efb792
JoaoMarcos5 <lucrandoo97@gmail.com>
2025-04-04 15:57:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from src.extrectTransform import requestApiBcb
-from src.load import salvarCsv
+from src.load import salvarCsv, salvarSqlite, salvarMySql
 
-dadosBcb = requestApiBcb('20191')
-salvarCsv(dadosBcb,'src/datasets/meiospagamentostri.csv', ";", ".")
+dadosBcb = requestApiBcb("20191")
+# salvarCsv(dadosBcb,'src/datasets/meiospagamentostri.csv', ";", ".")
+# salvarSqlite(dadosBcb,"src/datasets/ETLBCB.db","meios_pagamentos_tri")
+
+salvarMySql(dadosBcb, "root", "root", "localrost", "etlBCB", "meios_pagamentos_tri")
