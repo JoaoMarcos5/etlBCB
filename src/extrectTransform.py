08d434e2221b6ee2f0d589c2054897dc86f510fd
JoaoMarcos5 <lucrandoo97@gmail.com>
2025-04-11 16:28:10 -0300

    aula 4

--- src/extrectTransform.py
+++ src/extrectTransform.py
@@ -3,15 +3,7 @@ import pandas as pd
 
 
 def requestApiBcb(data) -> pd.DataFrame:
-    """
-    função para extrair os dados do meio de pagamento trimestrais do banco central
-    parâmetros:
 
-    data - string - aaat(exemplo 20191)
-
-    saída:
-    Dataframe - estrutura de dados do pandas
-    """
     url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
     req = requests.get(url)
     print("status code", req.status_code)
