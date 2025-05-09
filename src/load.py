67ef077d23136d3e26984f8a2428d5fae1a80d4b
JoaoMarcos5 <lucrandoo97@gmail.com>
2025-05-09 16:05:19 -0300

    Aula-6
    ultima atualização do codigo

--- src/load.py
+++ src/load.py
@@ -3,45 +3,26 @@ import sqlite3
 from sqlalchemy import create_engine
 
 
-def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str):
-    """
-    Função criada para salvar o dataframe como um arquivo csv.
-
-    Parameentro:
-        df(pd.DataFrame): o datafrana que deve ser salvo.
-        nome_arquivo(str): caminho e nome do arquivo de saída (ex:'dados.csv').
-        separador(str): caractre usado para reprensentar o ponto decimal (ex:'.' ou ',').
-        decimal(str): caractere usado para representar o ponto decimal(ex: '.' ou ',').
-
-        saida esperada:
-        o Dataframe deve ser salvo em um arquivo.csv
-
-
-
-
-
-
-
-    """
-    df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
+def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, dec: str):
+    df.to_csv(nome_arquivo, sep=separador, decimal=dec)
     return
 
 
-def salvarSqlite(df: pd.DataFrame, nome_banco: str, nome_tabela: str):
-
-
-
+def salvarSqlite(df: pd.DataFrame, nome_banco: str, nome_tabela):
     conn = sqlite3.connect(nome_banco)
-
     df.to_sql(nome_tabela, conn, if_exists="replace", index=False)
     conn.close()
-
     return
 
 
 def salvarMySql(
-    df: pd.DataFrame, usuario: str, senha: str, host: str, banco: str, nome_tabela: str
+    df: pd.DataFrame,
+    usuario: str,
+    senha: str,
+    host: str,
+    nome_banco: str,
+    nome_tabela: str,
 ):
-    engine = create_engine(f"mysql+pysmysql://{usuario}:{senha}@{host}:{banco}")
-
+    engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@{host}/{nome_banco}")
     df.to_sql(nome_tabela, con=engine, if_exists="replace", index=False)
+    return
